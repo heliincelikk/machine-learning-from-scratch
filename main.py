4c3f76042a499369fb0cb3d073b3c7d97ab84fba
heliincelikk <helincelik3403@gmail.com>
2026-08-24 03:08:30 +0300

    Train first linear regression model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 veriler = {
     "calisma_saati": [1, 2, 3, 4, 5],
@@ -8,3 +9,20 @@ veriler = {
 df = pd.DataFrame(veriler)
 
 print(df)
+
+X = df[["calisma_saati"]]
+y = df["sinav_puani"]
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+yeni_veri = pd.DataFrame(
+    {"calisma_saati": [6]}
+)
+
+tahmin = model.predict(yeni_veri)
+
+print("Tahmin edilen sınav puanı:", tahmin[0])
+print("Katsayı:", model.coef_[0])
+print("Sabit:", model.intercept_)
